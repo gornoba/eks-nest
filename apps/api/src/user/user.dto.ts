f848c3d785e92e1ce0a9da60d7e2c20e8d13db01
HyunHoo Im <gornoba@naver.com>
2024-06-09 14:42:00 +0900

    ebs

--- apps/api/src/user/user.dto.ts
+++ apps/api/src/user/user.dto.ts
@@ -1,20 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({
+    example: 'test@test.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: '12345',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
 }
 
 export class UserUpdateDto {
+  @ApiProperty({
+    example: 'tes2t@test.com',
+  })
   @IsOptional()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: '23456',
+  })
   @IsOptional()
   @IsString()
   password: string;
--- libs/common/src/health-check/health-check.controller.ts
+++ libs/common/src/health-check/health-check.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthCheckController {
   @Get()
   healthChcek() {
-    return true;
+    return process.env.HOSTNAME;
   }
 }
